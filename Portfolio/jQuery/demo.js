04972980e398599150617f8130231262ac3de5dc
Dario Costanzo <dariocostanzo@gmail.com>
2018-12-23 08:42:59 +0000

    My Porfolio

---
+++ Portfolio/jQuery/demo.js
@@ -0,0 +1,14 @@
+//Select all divs and give them a purple background
+$("div").css("background", "purple");
+
+//Select the divs with class "highlight" and make them 200px width
+$("div.highlight").css("width", "200px");
+
+//Select the div with id "third" and giv it a orange border
+$("#third").css("border", "4px solid orange");
+
+//Select the first div only and change its font color to pink
+$("div:first").css("color","pink");
+
+
+
